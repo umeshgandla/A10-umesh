e60effbdf41f1dc01f74b16d224c5f823a9a9715
umeshgandla <139842606+umeshgandla@users.noreply.github.com>
2024-11-13 22:11:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 def hello():
-    return "Hello from flask in docker"
+    return "Assignment: CI/CD Pipeline with Jenkins Master in Docker, Jenkins Slave on EC2, IAM Role-Based Access, Multi-Environment Docker Deployment, and Email Notification"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
